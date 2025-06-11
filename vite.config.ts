d1c2eb517a81a78260d93972fdb23432b72946b0
ombima56 <ombimahillary6@gmail.com>
2025-06-11 14:55:35 +0300

    Add server configuration to allow specific hosts in Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		allowedHosts: ["likely-true-panther.ngrok-free.app"],
+	},
 });
